91973c8ba9a7b356f0d8c8997a9ea8ec31d1476d
Scottie Enriquez <scottie@enriquez.xxx>
2020-07-05 18:32:29 -0500

    Adding base navigation structure

---
+++ Random Colors/Random Colors/ColorsDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ColorsDetailViewController.swift
+//  Random Colors
+//
+//  Created by Scott Enriquez on 7/5/20.
+//  Copyright © 2020 Scott Enriquez. All rights reserved.
+//
+
+import UIKit
+
+class ColorsDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Random Colors/Random Colors/ColorsTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ColorsTableViewController.swift
+//  Random Colors
+//
+//  Created by Scott Enriquez on 7/5/20.
+//  Copyright © 2020 Scott Enriquez. All rights reserved.
+//
+
+import UIKit
+
+class ColorsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func tempButtonTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "ToColorDetailsViewController", sender: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
